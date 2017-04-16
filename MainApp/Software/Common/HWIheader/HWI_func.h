019f6400be5e7e182bcce71d4168ec70753c80a2
aimbaby <imbaby0ahmed@gmail.com>
2017-04-16 21:43:57 +0200

    change of HWI_TIMERS_INITIALIZE to HWI_INTERRUPTS_INTITALIZE

--- MainApp/Software/Common/HWIheader/HWI_func.h
+++ MainApp/Software/Common/HWIheader/HWI_func.h
@@ -1,4 +1,4 @@
-PUBLIC void HWI_TIMERS_INTITALIZE(void);
+PUBLIC void HWI_INTERRUPTS_INTITALIZE(void);
 PUBLIC void HWI_DIGITAL_INITIALIZE(void);
 PUBLIC unsigned char HWI_DIGI_READ(unsigned char pin);
 PUBLIC void HWI_DIGI_WRITE(unsigned char pin,unsigned char value);
